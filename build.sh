2008875818a98f6e28ae49770561a9081bc3a5a1
Jaikrishna-M <154756917+Jaikrishna-M@users.noreply.github.com>
2024-05-28 16:18:17 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+echo "Inside the build file executing the commands"
 image_name="my-react"
 image_tag="latest"
 check_val=0
-echo "Inside the build file executing the commands"
+
 docker login -u jaikrishnam -p zendevops#123
 
 docker build -t $image_name:$image_tag .
 
-if [ $? -eq 0 ]; then
-    echo "Docker image build successful: $IMAGE_NAME:$IMAGE_TAG"
-else
-    echo "Docker image build failed"
-    exit 1
-fi
+#if [ $? -eq 0 ]; then
+#    echo "Docker image build successful: $IMAGE_NAME:$IMAGE_TAG"
+#else
+#    echo "Docker image build failed"
+#    exit 1
+#fi
 
