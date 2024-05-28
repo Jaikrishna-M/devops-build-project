16a6313b3401a6c64fbdeda9fc18af1a2af99423
Jaikrishna-M <154756917+Jaikrishna-M@users.noreply.github.com>
2024-05-28 22:51:51 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+        docker tag my-react jaikrishnam/development
+        docker push jaikrishnam/development
+        
+else
+        echo "failed"
+fi
 
