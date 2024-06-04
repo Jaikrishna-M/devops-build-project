0eca674d8ff6cbe4c59187d9326e7bf908c754cd
Jaikrishna-M <154756917+Jaikrishna-M@users.noreply.github.com>
2024-06-04 19:31:09 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ if [[ $GIT_BRANCH == "origin/development" ]]; then
         docker tag my-react jaikrishnam/development
         docker push jaikrishnam/development
         
-if [[ $GIT_BRANCH == "origin/main" ]]; then
+if [[ $GIT_BRANCH == "origin/prod" ]]; then
         docker tag my-react jaikrishnam/production
         docker push jaikrishnam/main
 else
